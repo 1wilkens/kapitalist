5f9dd59a120eb0114415eba7fc3e19f07b4140b4
Florian Wilkens <gh@1wilkens.org>
2018-04-27 09:04:14 +0200

    [README.md] Update Readme.md

---
+++ src/endpoint/category.rs
@@ -0,0 +1,20 @@
+use rocket_contrib::Json;
+
+use auth::UserGuard;
+use db::DbConn;
+
+use model::{Category};
+use request::*;
+use response::*;
+
+#[get("/<id>")]
+pub fn get(db: DbConn, _user: UserGuard, id: i32) -> Result<Json<Category>, Json<ErrorResponse>> {
+    use schema::categories;
+
+    let c = categories::table
+        .filter(categories::columns::id.eq(id))
+        .get_result(&*db)
+        .map_err(|_| Json(ErrorResponse::server_error()))?;
+
+    Ok(Json(c))
+}
