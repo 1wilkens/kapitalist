b3c27fddbd6962135675807135d6eb4a6667dbef
Florian Wilkens <gh@1wilkens.org>
2018-08-09 21:30:33 +0200

    [db/executor] Introduce proper error types to actix message handling

--- src/db/executor.rs
+++ src/db/executor.rs
@@ -1,4 +1,7 @@
-use actix_web::actix::{Actor, Handler, Message, SyncContext};
+use actix_web::{
+    actix::{Actor, Handler, Message, SyncContext},
+    error::{self, Error},
+};
 
 use diesel::{self, prelude::*};
 
@@ -21,42 +24,41 @@ impl DatabaseExecutor {
 pub struct GetPGVersion;
 
 impl Message for GetPGVersion {
-    type Result = Result<String, ()>;
+    type Result = Result<String, Error>;
 }
 
 impl Handler<GetPGVersion> for DatabaseExecutor {
-    type Result = Result<String, ()>;
+    type Result = Result<String, Error>;
 
     fn handle(&mut self, _msg: GetPGVersion, _: &mut Self::Context) -> Self::Result {
-        Err(())
+        Ok("".to_string())
     }
 }
 
 impl Message for NewUser {
-    type Result = Result<User, String>;
+    type Result = Result<User, Error>;
 }
 
 impl Handler<NewUser> for DatabaseExecutor {
-    type Result = Result<User, String>;
+    type Result = Result<User, Error>;
 
     fn handle(&mut self, msg: NewUser, _: &mut Self::Context) -> Self::Result {
         use db::schema::users::dsl::*;
 
         // XXX: Figure out error type to be used here and add conversion functions for convenience
-        let exists: bool = diesel::select(diesel::dsl::exists(users
-            .filter(email.eq(&msg.email))))
+        let exists: bool = diesel::select(diesel::dsl::exists(users.filter(email.eq(&msg.email))))
             .get_result(&self.0)
-            .map_err(|_| "Database error".to_string())?;
+            .map_err(|_| error::ErrorInternalServerError("Error getting user"))?;
 
         if exists {
             // TODO: should we really return this message?
-            return Err("User already exists".into());
+            return Err(error::ErrorUnauthorized("User already exists"));
         }
 
         let user: User = diesel::insert_into(users)
             .values(&msg)
             .get_result(&self.0)
-            .map_err(|_| "Database error".to_string())?;
+            .map_err(|_| error::ErrorInternalServerError("Error inserting user"))?;
         Ok(user)
     }
 }
@@ -64,11 +66,11 @@ impl Handler<NewUser> for DatabaseExecutor {
 pub struct GetUser(pub String);
 
 impl Message for GetUser {
-    type Result = Result<User, String>;
+    type Result = Result<User, Error>;
 }
 
 impl Handler<GetUser> for DatabaseExecutor {
-    type Result = Result<User, String>;
+    type Result = Result<User, Error>;
 
     fn handle(&mut self, msg: GetUser, _: &mut Self::Context) -> Self::Result {
         use db::schema::users::dsl::*;
@@ -77,7 +79,7 @@ impl Handler<GetUser> for DatabaseExecutor {
         let user: User = users
             .filter(email.eq(&msg.0))
             .get_result(&self.0)
-            .map_err(|_| "Database error".to_string())?;
+            .map_err(|_| error::ErrorInternalServerError("Error getting user`"))?;
         Ok(user)
     }
 }
