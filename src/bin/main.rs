90bfb388c44bf512f6656efc6e1264a2ceb318a9
Florian Wilkens <gh@1wilkens.org>
2018-08-29 15:18:00 +0200

    [state] Move host, port and db_url into Config

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -23,10 +23,10 @@ use slog::Logger;
 
 /// Required environment variables for kapitalist
 ///
-/// - "KAPITALIST_HOST"       - Which IP address to listen on
-/// - "KAPITALIST_PORT"       - Which port to listen on
-/// - "KAPITALIST_DB"         - Connection string of the backing database (diesel format)
-/// - "KAPITALIST_JWT_SECRET" - JWT secret to sign tokens with
+/// - KAPITALIST_HOST       - Which IP address to listen on
+/// - KAPITALIST_PORT       - Which port to listen on
+/// - KAPITALIST_DB         - Connection string of the backing database (diesel format)
+/// - KAPITALIST_JWT_SECRET - JWT secret to sign tokens with
 static REQUIRED_VARIABLES: [&str; 4] = [
     "KAPITALIST_HOST",
     "KAPITALIST_PORT",
@@ -34,7 +34,7 @@ static REQUIRED_VARIABLES: [&str; 4] = [
     "KAPITALIST_JWT_SECRET"
 ];
 
-fn parse_env() {
+fn load_env() {
     for item in dotenv::dotenv_iter().unwrap() {
         if let Ok((key, val)) = item {
             if let Err(env::VarError::NotPresent) = env::var(&key) {
@@ -44,7 +44,7 @@ fn parse_env() {
     }
 }
 
-fn check_env(log: &Logger) -> bool {
+fn check_env(log: &Logger) -> Result<(), String> {
     trace!(log, "Cheking environment");
     let vars: HashMap<String, String> = env::vars().collect();
 
@@ -53,50 +53,59 @@ fn check_env(log: &Logger) -> bool {
             debug!(log, "Found required env variable"; "name" => v, "value" => &vars[*v]);
         } else {
             debug!(log, "Missing required env variables"; "name" => v);
-            return false;
+            return Err(v.to_string());
         }
     }
 
-    true
+    Ok(())
 }
 
 fn main() {
     // logging log
     let log = init_logging();
 
-    parse_env();
-    if !check_env(&log) {
-        // XXX: Improve format and print environment variables found and missing
-        error!(log, "Failed to validate environment.\nPlease check all required variables are present and valid\nExiting..");
+    // XXX: add help flag (debate whether to use a crate for this)
+
+    // load and check environment
+    load_env();
+    if let Err(var) = check_env(&log) {
+        error!(log, "Failed to validate environment"; "missing" => var);
         return;
     }
 
-    let addr = "0.0.0.0:3000";
-    let db_url = env::var("KAPITALIST_DB").unwrap();
+    let cfg = match Config::from_env() {
+        Ok(cfg) => cfg,
+        Err(e)  => {
+            error!(log, "Failed to parse configuration"; "error" => ?e);
+            return;
+        }
+    };
+    let cfg_ = cfg.clone();
 
     // actix main system
     let sys = actix::System::new("kapitalist");
 
+
     // configuration + database connection
-    let cfg = Config::from_env().unwrap();
     let db = actix::SyncArbiter::start(3, move || {
-        DatabaseExecutor::new(&db_url).expect("Failed to instantiate DatabaseExecutor")
+        DatabaseExecutor::new(&cfg_.db_url.clone()).expect("Failed to instantiate DatabaseExecutor")
     });
 
-    let tmp = log.clone();
+    let log_ = log.clone();
+    let cfg_ = cfg.clone();
     server::new(move || {
-        let state = AppState::new(cfg.clone(), db.clone());
+        let state = AppState::new(cfg_.clone(), db.clone());
         App::with_state(state)
-            .middleware(SlogLogger::new(tmp.clone()))
+            .middleware(SlogLogger::new(log_.clone()))
             .resource("/", |r| r.get().f(api::index))
             .resource("/register", |r| r.post().with(api::user::register))
             .resource("/token", |r| r.post().with(api::user::token))
             .resource("/me", |r| r.get().with(api::user::get_me))
-    }).bind(&addr)
+    }).bind(&cfg.addr.clone())
     .expect("Failed to start server")
     .start();
 
-    info!(log, "Started server on: {}", &addr);
+    info!(log, "Started server on: {}", &cfg.addr);
     let _ = sys.run();
 }
 
--- src/state.rs
+++ src/state.rs
@@ -1,28 +1,43 @@
 use auth::JwtSecret;
 use db::DatabaseExecutor;
 
-use std::env;
+use std::convert::From;
+use std::{env, net};
 
 use actix_web::actix::Addr;
 
 #[derive(Debug, Clone)]
 pub struct Config {
-    pub(crate) jwt_secret: JwtSecret,
+    pub addr: net::SocketAddr,
+    pub jwt_secret: JwtSecret,
+    pub db_url: String,
 }
 
-impl Config {
-    pub fn from_env() -> Option<Config> {
-        let jwt_secret = env::var("KAPITALIST_JWT_SECRET").ok()?;
-        Some(Config{
-            jwt_secret: JwtSecret(jwt_secret)
-        })
-    }
-}
-
-// XXX: Maybe implement debug for this?
+// XXX: Maybe implement debug for this
 pub struct AppState {
     pub(crate) config: Config,
     pub(crate) db: Addr<DatabaseExecutor>,
+}
+
+// XXX: Maybe use failure here
+#[derive(Debug, Clone)]
+pub enum ParseError {
+    InvalidEnvironment(env::VarError),
+    InvalidAddress(net::AddrParseError),
+}
+
+impl Config {
+    pub fn from_env() -> Result<Config, ParseError> {
+        let jwt_secret = env::var("KAPITALIST_JWT_SECRET")?;
+        let addr = env::var("KAPITALIST_HOST")? + ":" + &env::var("KAPITALIST_PORT")?;
+        let addr: net::SocketAddr = addr.parse()?;
+        let db_url = env::var("KAPITALIST_DB")?;
+        Ok(Config {
+            addr: addr,
+            jwt_secret: JwtSecret(jwt_secret),
+            db_url: db_url,
+        })
+    }
 }
 
 impl AppState {
@@ -31,5 +46,17 @@ impl AppState {
             config: cfg,
             db: addr,
         }
+    }
+}
+
+impl From<env::VarError> for ParseError {
+    fn from(error: env::VarError) -> ParseError {
+        ParseError::InvalidEnvironment(error)
+    }
+}
+
+impl From<net::AddrParseError> for ParseError {
+    fn from(error: net::AddrParseError) -> ParseError {
+        ParseError::InvalidAddress(error)
     }
 }
