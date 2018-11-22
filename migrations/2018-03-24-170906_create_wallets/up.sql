4f62f3cb9cef3e9513804a195d0e80a4a84df3e0
Florian Wilkens <gh@1wilkens.org>
2018-11-22 10:10:45 +0100

    [migrations] Add proper foreign key to wallets and fix indentation

--- migrations/2018-03-24-170906_create_wallets/up.sql
+++ migrations/2018-03-24-170906_create_wallets/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE wallets (
     id              SERIAL NOT NULL PRIMARY KEY,
-    user_id         INTEGER NOT NULL,
+    user_id         INTEGER NOT NULL REFERENCES users(id),
     name            TEXT NOT NULL,
     initial_balance INTEGER NOT NULL,
     current_balance INTEGER NOT NULL,
--- migrations/2018-03-26-173207_create_categories/up.sql
+++ migrations/2018-03-26-173207_create_categories/up.sql
@@ -2,5 +2,5 @@ CREATE TABLE categories (
     id              SERIAL NOT NULL PRIMARY KEY,
     name            TEXT NOT NULL,
     color           TEXT,
-    created_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
