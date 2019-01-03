cd560b646f25ca8fbea0ba8cf263a796cb474cf0
Florian Wilkens <gh@1wilkens.org>
2019-01-03 11:14:18 +0100

    [FEAT] Add some more stock categories

--- migrations/2018-03-26-173207_create_categories/up.sql
+++ migrations/2018-03-26-173207_create_categories/up.sql
@@ -9,4 +9,16 @@ CREATE TABLE categories (
 
 -- Insert stock categories
 INSERT INTO categories (parent_id, user_id, name, color)
-VALUES (NULL, NULL, "Shopping", "0x0022DD");
+VALUES (NULL, NULL, 'Shopping', '0x0022DD');
+
+INSERT INTO categories (parent_id, user_id, name, color)
+VALUES (NULL, NULL, 'Foods & Drinks', '0x00DD22');
+
+INSERT INTO categories (parent_id, user_id, name, color)
+VALUES (NULL, NULL, 'Housing', '0x0044AA');
+
+INSERT INTO categories (parent_id, user_id, name, color)
+VALUES (NULL, NULL, 'Life & Entertainment', '0x00AA44');
+
+INSERT INTO categories (parent_id, user_id, name, color)
+VALUES (NULL, NULL, 'Misc', '0x22AA22');
