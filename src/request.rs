d36a431e528c8175f029c3b9259d9624dd09c95b
Florian Wilkens <gh@1wilkens.org>
2019-01-10 13:18:01 +0100

    [FIX] Make Options in requests really optional

--- src/request.rs
+++ src/request.rs
@@ -5,13 +5,17 @@ use serde::Deserialize;
 pub struct UserCreationRequest {
     pub email: String,
     pub password: String,
+    #[serde(default)]
     pub name: Option<String>,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct UserUpdateRequest {
+    #[serde(default)]
     pub email: Option<String>,
+    #[serde(default)]
     pub password: Option<String>,
+    #[serde(default)]
     pub name: Option<String>,
 }
 
@@ -32,6 +36,7 @@ pub struct WalletCreationRequest {
 #[derive(Debug, Deserialize)]
 pub struct CategoryCreationRequest {
     pub name: String,
+    #[serde(default)]
     pub parent_id: Option<i32>,
     pub color: String,
 }
