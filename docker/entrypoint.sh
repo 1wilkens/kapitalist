807188f3d99b055be0fffaeeb14fe24cc6d7881b
Florian Wilkens <gh@1wilkens.org>
2020-06-13 23:43:27 +0200

    [FIX] Don't hardcode database hostname in docker entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 set -eox pipefail
 
 # wait for postgres to be available
-wait-for-port.sh db:5432 -t 0 -q
+wait-for-port.sh "${KAPITALIST_DB_HOST:=db}:5432" -t 0 -q
 
 # init database and start serving
 # XXX: init should only be run on first launch, how can we manage that?
